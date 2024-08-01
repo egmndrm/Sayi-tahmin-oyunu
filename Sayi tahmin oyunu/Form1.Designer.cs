85f2c28de4b075bd4d83f99b8e2a731a7c9c5582
Egemen <Egemen@DESKTOP-T6LPQB8>
2024-08-01 18:54:28 +0300

    Proje dosyası ekle.

---
+++ Sayi tahmin oyunu/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Sayi_tahmin_oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(217, 248);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "kalan hak : 3\r\n";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(182, 153);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(141, 56);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Tahmin et";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(185, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0-100 arasi tahmini kutuya yaziniz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Sayi tahmin oyunu/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sayi_tahmin_oyunu
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        int rastgele,tahmin , hak = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hak--;
+            tahmin = Convert.ToInt32(textBox1.Text);
+            if (rastgele == tahmin)
+            {
+
+                MessageBox.Show("Tebrikler Bildiniz");
+                hak = 3;
+                label1.Text = "Hak=" + hak;
+            }
+
+
+            else
+            {
+                MessageBox.Show("Uzgunum Bilemediniz");
+                label1.Text = "Hak=" + hak;
+                
+                if(hak == 0)
+                {
+                    MessageBox.Show("Hakkiniz kalmamistir");
+                    textBox1.Enabled = false;
+                    btn1.Enabled = false;
+                }
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rastgele = rnd.Next(0, 100);
+            
+        }
+    }
+}
